d1f705c30ecb72dc510b1b4c463fda77408e7d8a
weizhang1994 <weiz6788@gmail.com>
2018-03-01 20:35:56 -0600

    The custom php frame work

---
+++ buttormvc copy/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'YOUR_USER');
+  define('DB_PASS', 'YOUR_PASS');
+  define('DB_NAME', 'YOUR_NAME');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'yourlocalhost');
+  // Site Name
+  define('SITENAME', 'ButtorMVC');
